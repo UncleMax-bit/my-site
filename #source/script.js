7cb8c41cc634a132ab0f42b1ae65895f34a73a65
UncleMax-bit <80449357+UncleMax-bit@users.noreply.github.com>
2021-07-01 10:08:00 +0300

    Bike-site

---
+++ #source/script.js
@@ -0,0 +1,27 @@
+
+    let ibg = document.querySelectorAll(".ibg"); 
+    for (var i = 0; i < ibg.length; i++) 
+    { 
+        if(ibg[i].querySelector('img'))
+        {
+            ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')'; 
+        } 
+    }
+
+    $(document).on('ready', function() {
+        $(".lazy").slick({
+            dots: true,
+            arrows: false,
+            autoplay: false,
+            adaptiveHeight: true,
+            lazyLoad: 'ondemand', // ondemand progressive anticipated
+            infinite: true
+        });
+
+        $('.header__burger').click(function(){
+            $(this).toggleClass('active');
+            $('.header__menu').toggleClass('active');
+            $('body').toggleClass('lock');
+        })
+    });
+        
